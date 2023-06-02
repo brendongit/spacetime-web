90c085a368fe9442ad3071f7c8865df39c8794b4
brendongit <brenddesigneroficial@gmail.com>
2023-06-02 15:18:41 -0300

    components - page

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright(){
+  return (
+    <div>
+    © Copyright -{" "}
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.linkedin.com/in/brendon-oliveira-0b467923a/"
+      className="underline hover:text-gray-50"
+    >
+      Rocketseat
+    </a>
+  </div>
+  )
+}
