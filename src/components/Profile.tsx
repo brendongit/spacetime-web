5191520dafe08edb66da4312e5ff35af3db64213
brendongit <brenddesigneroficial@gmail.com>
2023-06-06 17:19:47 -0300

    logout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,7 @@
 import { getUser } from "@/lib/auth";
 import Image from "next/image";
-export function Profile(){
 
+export function Profile(){
   const { name, avatarUrl} = getUser()
 
   return (
@@ -9,7 +9,7 @@ export function Profile(){
           <Image src={avatarUrl} width={40} height={40} alt="" className="w-10 h-10 rounded-full"/>
           <p className="max-w-[140px] text-sm leading-snug">
             {name}
-            <a href="" className="block text-red-400 hover:text-red-300">Quero sair</a>
+            <a href="/api/auth/logout" className="block text-red-400 hover:text-red-300">Quero sair</a>
           </p>
         </div>
   )
